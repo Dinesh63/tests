14f0cc82e3d46b5d2bc117459540c3fba6c7e9b3
Dinesh Godara <DineshGodara@fico.com>
2018-10-19 18:35:53 +0530

    commit from eclipse

---
+++ src/test2/TestClass.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+* This material is the confidential, proprietary, unpublished property
+
+* of Fair Isaac Corporation.  Receipt or possession of this material
+* does not convey rights to divulge, reproduce, use, or allow others
+* to use it without the specific written authorization of Fair Isaac
+* Corporation and use must conform strictly to the license agreement.
+* 
+ * Copyright (c) 2018 Fair Isaac Corporation.  All rights reserved.
+******************************************************************************/
+
+package test2;
+
+public class TestClass {
+
+}
